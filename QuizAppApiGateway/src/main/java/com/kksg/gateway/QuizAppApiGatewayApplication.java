cb5218d593874ff7c584de6ccc89ba94d470ef45
codewithkrishan2 <codewithkrishan@gmail.com>
2024-02-07 18:49:22 +0530

    API-Gateway-configuration

---
+++ QuizAppApiGateway/src/main/java/com/kksg/gateway/QuizAppApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kksg.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppApiGatewayApplication.class, args);
+	}
+
+}
